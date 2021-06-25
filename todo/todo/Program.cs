91bf0446dbe4db5ce7487d40bbc3c6dea77ad83a
Phil Whittington <philipwhittington@gmail.com>
2021-06-25 20:17:19 +0100

    Initial add of project and test in Rider

---
+++ todo/todo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace todo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
