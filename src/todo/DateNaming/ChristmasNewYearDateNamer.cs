e9808d5b799bab287956d8731e6cfbf8f17fca1f
Phil Whittington <philipwhittington@gmail.com>
2021-12-30 07:02:23 +0000

    Re-ordering switch statement

--- src/todo/DateNaming/ChristmasNewYearDateNamer.cs
+++ src/todo/DateNaming/ChristmasNewYearDateNamer.cs
@@ -8,12 +8,12 @@ public class ChristmasNewYearDateNamer : IChristmasNewYearDateNamer
     {
         name = date switch
         {
-            { Month: 1, Day: 1 } => "New Year's Day",
-            { Month: 1, Day: 6 } => "Epiphany",
             { Month: 12, Day: 24 } => "Christmas Eve",
             { Month: 12, Day: 25 } => "Christmas Day",
             { Month: 12, Day: 26 } => "Boxing Day",
             { Month: 12, Day: 31 } => "New Year's Eve",
+            { Month: 1, Day: 1 } => "New Year's Day",
+            { Month: 1, Day: 6 } => "Epiphany",
             _ => null
         };
 
