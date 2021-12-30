6f9253ab1bb8343168c926b1980fc3be9ee0e8a1
Phil Whittington <philipwhittington@gmail.com>
2021-12-30 07:38:41 +0000

    Adding browser and text editor paths to Configuration file

--- src/todo/Contracts/Data/ConfigurationInfo.cs
+++ src/todo/Contracts/Data/ConfigurationInfo.cs
@@ -4,15 +4,23 @@ namespace Todo.Contracts.Data
 {
     public class ConfigurationInfo
     {
+        public string BrowserPath { get; }
+
+        public string TextEditorPath { get; }
+        
         public string TemplatePath { get; }
         
         public bool UseNamesForDays { get; }
 
         [JsonConstructor]
-        public ConfigurationInfo(string templatePath, bool useNamesForDays)
+        public ConfigurationInfo(string templatePath, 
+            bool useNamesForDays, string browserPath,
+            string textEditorPath)
         {
             TemplatePath = templatePath;
             UseNamesForDays = useNamesForDays;
+            BrowserPath = browserPath;
+            TextEditorPath = textEditorPath;
         }
     }
 }
