14a647322e08160d0e4e6533fc1603ac7080273a
Phil Whittington <philipwhittington@gmail.com>
2021-12-30 12:05:54 +0000

    Harmonise class name

---
+++ src/todo/State/CommandProvider.cs
@@ -0,0 +1,21 @@
+﻿using Todo.Contracts.Data.Commands;
+using Todo.Contracts.Services;
+
+namespace Todo.State
+{
+    public class CommandProvider : ICommandProvider
+    {
+        private readonly ICommandLineParser _commandLineParser;
+
+        public CommandProvider(ICommandLineParser commandLineParser)
+        {
+            _commandLineParser = commandLineParser;
+        }
+        
+        public CommandBase GetCommand()
+        {
+            var date = _commandLineParser.GetDateFromCommandLine();
+            return CreateOrShowCommand.Of(date);
+        }
+    }
+}
