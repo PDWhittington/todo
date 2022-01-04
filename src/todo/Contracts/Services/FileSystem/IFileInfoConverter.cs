4edea1b7bd2c81a91f327493b1347e5176f9d9fa
Phil Whittington <philipwhittington@gmail.com>
2022-01-04 00:05:02 +0000

    Introducing FileInfoConverter for better html filename conversion

---
+++ src/todo/Contracts/Services/FileSystem/IFileInfoConverter.cs
@@ -0,0 +1,8 @@
+﻿using Todo.Contracts.Data.FileSystem;
+
+namespace Todo.Contracts.Services.FileSystem;
+
+public interface IFileInfoConverter
+{
+    FilePathInfo Convert(FilePathInfo filePathInfo, FileTypeEnum fileTypeEnum);
+}
---
+++ src/todo/FileSystem/FileInfoConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Todo.Contracts.Data.FileSystem;
+using Todo.Contracts.Services.FileSystem;
+
+namespace Todo.FileSystem;
+
+public class FileInfoConverter : IFileInfoConverter
+{
+    public FilePathInfo Convert(FilePathInfo filePathInfo, FileTypeEnum fileTypeEnum)
+    {
+        var newExtension = fileTypeEnum switch
+        {
+            FileTypeEnum.Html => "html",
+            FileTypeEnum.Markdown => "md",
+        };
+
+        var newPath = Path.ChangeExtension(filePathInfo.Path, "");
+
+        throw new System.NotImplementedException();
+    }
+}
