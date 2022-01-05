4c5ecb6d3e074ba8d7c604f723e3043edd235bc6
Phil Whittington <philipwhittington@gmail.com>
2022-01-05 05:53:43 +0000

    Adding some notes beneath the command table

--- src/todo/Execution/ShowHelpExecutor.cs
+++ src/todo/Execution/ShowHelpExecutor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using Todo.Contracts.Data.Commands;
 using Todo.Contracts.Data.HelpMessages;
 using Todo.Contracts.Services.CommandFactories;
@@ -25,7 +26,35 @@ public class ShowHelpExecutor : IShowHelpExecutor
             .GetAllCommandFactories()
             .Select(cf => new CommandHelpMessage(cf.CommandWords.ToArray(), cf.HelpText));
 
-        var table = _tableWriter.CreateTable(commandHelpMessages);
-        Console.WriteLine(table);
+        var sb = new StringBuilder()
+            .AppendLine(_tableWriter.CreateTable(commandHelpMessages))
+            .AppendLine()
+            .AppendLine(GetNotes());
+
+        Console.WriteLine(sb);
     }
+
+    private static string GetNotes() => _notes
+        .Replace("->", "\u2192");
+
+    private const string _notes =
+@"Notes: 
+
+createorshow is the default command. This means it can be accessed simply by typing anything that can be parsed as a date after the word todo.
+
+Valid date formats:-
+
+    ""y"", ""yesterday"" -> yesterday 
+    [empty string], ""."", ""today"" -> today
+    ""tm"", ""tomorrow"" -> tomorrow
+    
+    [day]/[month] -> maps to the day/month which is nearest in time to today
+    [day] -> maps to the day/month/year which is nearest in time to today
+    +[daycount] -> positive offset a number of days from today
+    -[daycount] -> negative offset a number of days from today
+
+[Commit Message] -> In the Commit and Sync commands, the commit message is optional. If none is supplied, then 
+a standard message detailing date and time of the commit will be used. 
+";
+
 }
