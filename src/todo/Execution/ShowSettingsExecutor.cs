0d27be20008f064da09fd141c37e99ff909c815f
Phil Whittington <philipwhittington@gmail.com>
2022-01-17 13:02:35 +0000

    Adding show settings logic to the app

---
+++ src/todo/Execution/ShowSettingsExecutor.cs
@@ -0,0 +1,25 @@
+﻿using Todo.Contracts.Data.Commands;
+using Todo.Contracts.Services.Execution;
+using Todo.Contracts.Services.FileLaunching;
+using Todo.Contracts.Services.StateAndConfig;
+
+namespace Todo.Execution;
+
+public class ShowSettingsExecutor: CommandExecutorBase<ShowSettingsCommand>, IShowSettingsExecutor
+{
+    private readonly ISettingsPathProvider _settingsPathProvider;
+    private readonly IFileOpener _fileOpener;
+
+    public ShowSettingsExecutor(ISettingsPathProvider settingsPathProvider, IFileOpener fileOpener)
+    {
+        _settingsPathProvider = settingsPathProvider;
+        _fileOpener = fileOpener;
+    }
+
+    public override void Execute(ShowSettingsCommand _)
+    {
+        var settingsPath = _settingsPathProvider.GetSettingsPath();
+
+        _fileOpener.LaunchFileInDefaultEditor(settingsPath.Path);
+    }
+}
