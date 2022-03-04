e3d78e4e08b2c050b89710f111912724e1eb022c
Phil Whittington <philipwhittington@gmail.com>
2022-03-04 21:42:51 +0000

    Changing the way the validation works in CommandFactorySet. User now gets all of the info to fix at once.

--- src/todo/CommandFactories/CommandFactorySet.cs
+++ src/todo/CommandFactories/CommandFactorySet.cs
@@ -31,6 +31,8 @@ public class CommandFactorySet : ICommandFactorySet
         out ICommandFactory<CommandBase> defaultCommandFactory,
         out ICommandFactory<CommandBase> [] nonDefaultCommandFactories)
     {
+        var exceptions = new List<Exception>();
+
         var commandFactoriesArr = commandFactories.ToArray();
 
         var wordsDuplicated = commandFactoriesArr
@@ -42,18 +44,32 @@ public class CommandFactorySet : ICommandFactorySet
 
         if (wordsDuplicated.Any())
         {
-            throw new Exception(
-                $"The following words are taken by more than one CommandFactory: {string.Join(',',wordsDuplicated)}");
+            exceptions.Add(new Exception($"The following words are taken by more " +
+                $"than one CommandFactory: {string.Join(',',wordsDuplicated)}"));
         }
 
+        var defaultCommandFactories = commandFactoriesArr
+            .Where(x => x.IsDefaultCommandFactory)
+            .ToArray();
 
-        var oneAndOnlyOneDefault = commandFactoriesArr
-            .Count(x => x.IsDefaultCommandFactory) == 1;
-
-        if (!oneAndOnlyOneDefault)
+        switch (defaultCommandFactories.Length)
         {
-            throw new Exception("There should be one and only one CommandFactory which is the default");
+            case 0:
+                exceptions.Add(new Exception($"There are no CommandFactory objects marked as " +
+                                             $"the default. {nameof(CommandFactoryBase<CommandBase>.IsDefaultCommandFactory)} " +
+                                             "should be true for one and only one class"));
+                break;
+            case > 1:
+                exceptions.Add(new Exception(
+                    "The following CommandFactory objects are marked as being the default CommandFactory: " +
+                    $"{string.Join(",", defaultCommandFactories.Select(x => x.GetType().Name))}. " +
+                    $"{nameof(CommandFactoryBase<CommandBase>.IsDefaultCommandFactory)} " +
+                    "should be true for one and only one class"));
+                break;
         }
+
+        if (exceptions.Any()) throw new AggregateException(
+            "A number of exceptions were thrown while validating the CommandFactory objects", exceptions);
 
         defaultCommandFactory = commandFactoriesArr
             .Single(x => x.IsDefaultCommandFactory);
