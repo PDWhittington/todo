2e08ba0dbaa8f49cfd0cdd97ccfb1111f7567dca
Philip Whittington <philipwhittington@gmail.com>
2022-03-12 02:48:22 +0000

    Changing the path to pick up the published artefacts (#155)

--- publish-linux.sh
+++ publish-linux.sh
@@ -5,5 +5,5 @@ dotnet publish src/todo/ -c Release -r linux-x64 -p:PublishReadyToRun=true --sel
 
 sudo rm -R /usr/local/bin/todo
 
-sudo cp -R src/todo/bin/Release/net6.0/publish /usr/local/bin/todo/
+sudo cp -R src/todo/bin/Release/net6.0/linux-x64/publish /usr/local/bin/todo/
 
