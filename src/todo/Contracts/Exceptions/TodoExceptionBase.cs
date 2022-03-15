c786ac48873f502c18841ccca3a71e0f54eace36
Philip Whittington <philipwhittington@gmail.com>
2022-03-15 21:21:51 +0000

    Make the app fail gracefully if a todo-settings.json is not found. (#161)

---
+++ src/todo/Contracts/Exceptions/TodoExceptionBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Todo.Contracts.Exceptions;
+
+public abstract class TodoExceptionBase : Exception
+{
+    protected TodoExceptionBase()
+        : base()
+    { }
+
+    protected TodoExceptionBase(string message)
+        : base(message)
+    { }
+
+    public abstract string Advice();
+}
--- src/todo/Git/GitInterface.cs
+++ src/todo/Git/GitInterface.cs
@@ -1,4 +1,5 @@
-﻿using LibGit2Sharp;
+﻿using System;
+using LibGit2Sharp;
 using Todo.Contracts.Services.FileSystem.Paths;
 using Todo.Contracts.Services.Git;
 using Todo.Contracts.Services.UI;
@@ -8,21 +9,27 @@ namespace Todo.Git;
 
 public class GitInterface : IGitInterface
 {
+    private readonly IOutputFolderPathProvider _outputFolderPathProvider;
     private readonly IOutputWriter _outputWriter;
-    private readonly IRepository _repository;
+    private readonly Lazy<IRepository> _repository;
 
     public GitInterface(IOutputFolderPathProvider outputFolderPathProvider,
         IOutputWriter outputWriter)
     {
+        _outputFolderPathProvider = outputFolderPathProvider;
         _outputWriter = outputWriter;
+        _repository = new Lazy<IRepository>(GetRepository);
+    }
 
-        var repoPath = Repository.Discover(outputFolderPathProvider.GetRootedOutputFolder());
-        _repository = new Repository(repoPath);
+    private IRepository GetRepository()
+    {
+        var repoPath = Repository.Discover(_outputFolderPathProvider.GetRootedOutputFolder());
+        return new Repository(repoPath);
     }
 
     public TResultType RunGitCommand<TCommandType, TResultType>(TCommandType command)
         where TCommandType : GitCommandBase<TResultType>
     {
-        return command.ExecuteCommand(_repository, _outputWriter);
+        return command.ExecuteCommand(_repository.Value, _outputWriter);
     }
 }
