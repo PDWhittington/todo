1b11f9564ac82643d631073c6fd3f20272aceba3
Phil Whittington <philipwhittington@gmail.com>
2022-03-15 23:26:14 +0000

    GitRemoveCommand improvements

--- src/todo/Git/Commands/GitRemoveCommand.cs
+++ src/todo/Git/Commands/GitRemoveCommand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using LibGit2Sharp;
 using Todo.Contracts.Services.UI;
 using Todo.Git.Results;
@@ -19,6 +21,22 @@ public class GitRemoveCommand : GitCommandBase<VoidResult>
     {
         try
         {
+            foreach (var path in Paths)
+            {
+                var indexEntries = repo
+                    .RetrieveStatus()
+                    .Where(x => x.State == FileStatus.NewInIndex)
+                    .Select(x => Path.Combine(repo.Info.WorkingDirectory, x.FilePath))
+                    .ToArray();
+
+                var pathInIndex = repo.Index.SingleOrDefault(
+                    x => path.Equals(Path.Combine(repo.Info.WorkingDirectory, x.Path)));
+
+                if (pathInIndex is null) continue;
+
+                repo.Index.Remove(pathInIndex.Path);
+            }
+
             LibGit2Sharp.Commands.Remove(repo, Paths);
             return new VoidResult(true, null);
         }
