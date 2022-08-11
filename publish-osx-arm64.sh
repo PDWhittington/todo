0f998597cf04254da5a85933fdd1aebb76606e3e
Philip Whittington <philipwhittington@gmail.com>
2022-08-11 22:20:22 +0100

    Create publish-osx-arm64.sh (#210)

---
+++ publish-osx-arm64.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# This is provided more as an example than anything else
+
+dotnet publish src/todo/ -c Release -r osx-arm64 -p:PublishReadyToRun=true --self-contained
+
+sudo rm -R -f /usr/local/bin/todo
+
+sudo cp -R src/todo/bin/Release/net6.0/osx-arm64/publish /usr/local/bin/todo/
+
