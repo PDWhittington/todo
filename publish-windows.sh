ab34c29a7d13ffe373e18032966e26396bff8dd8
Philip Whittington <philipwhittington@gmail.com>
2023-07-05 22:33:05 +0100

    Updating to .Net 7.0

--- publish-windows.sh
+++ publish-windows.sh
@@ -5,5 +5,5 @@ dotnet publish src/todo/ -c Release -r win-x64 -p:PublishReadyToRun=true --self-
 
 rm -R /c/portable/todo
 
-cp -R src/todo/bin/Release/net6.0/win-x64/publish /c/portable/todo/
+cp -R src/todo/bin/Release/net7.0/win-x64/publish /c/portable/todo/
 
