af9a2d336986d54edaf2912938b23565c705502f
Philip Whittington <philipwhittington@gmail.com>
2026-04-07 23:20:23 +0100

    Config switch to allow light and dark mode in html (#245)

--- src/TodoTests/CommandInterpretationTests.cs
+++ src/TodoTests/CommandInterpretationTests.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using Todo;
 using Todo.Contracts.Data.Commands;
 using Todo.Contracts.Data.Config;
+using Todo.Contracts.Data.Html;
 using Todo.Contracts.Services.Dates;
 using Todo.Contracts.Services.StateAndConfig;
 
@@ -44,7 +45,7 @@ public class CommandInterpretationTests
         var blankPaths = new PerOsFilePaths(blankProcessLaunchInfo, blankProcessLaunchInfo, blankProcessLaunchInfo);
         
         var configuration = new Configuration(blankPaths, blankPaths, "",
-            "", "", "", "", "",
+            "", "", HtmlThemeEnum.Dark, "", "", "",
             true, true, new TimeSpan(4, 0, 0), 
             80, IterationMethodEnum.Parallel);
 
--- src/todo/Contracts/Data/Substitutions/HtmlSubstitutions.cs
+++ src/todo/Contracts/Data/Substitutions/HtmlSubstitutions.cs
@@ -4,12 +4,16 @@ public class HtmlSubstitutions : SubstitutionsBase
 {
     public string Title { get; }
     public string Body { get; }
+    
+    public string Theme { get; }
 
-    private HtmlSubstitutions(string title, string body)
+    private HtmlSubstitutions(string title, string body, string theme)
     {
         Title = title;
         Body = body;
+        Theme = theme;
     }
 
-    public static HtmlSubstitutions Of(string title, string body) => new(title, body);
+    public static HtmlSubstitutions Of(string title, string body, string theme) 
+        => new(title, body, theme);
 }
