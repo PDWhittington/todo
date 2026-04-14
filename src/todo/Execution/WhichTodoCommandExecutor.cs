13941cb5b60e6ec074c5498451bc611269058c20
Phil Whittington <philipwhittington@gmail.com>
2026-04-15 00:36:39 +0100

    Removing duplicated code

--- src/todo/Execution/WhichTodoCommandExecutor.cs
+++ src/todo/Execution/WhichTodoCommandExecutor.cs
@@ -9,38 +9,11 @@ using Todo.Contracts.Services.UI;
 namespace Todo.Execution;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
-public class WhichTodoCommandExecutor : CommandExecutorBase<WhichTodoCommand>, IWhichTodoCommandExecutor
+public class WhichTodoCommandExecutor(
+    IOutputWriter outputWriter,
+    IBoilerPlateProvider boilerPlateProvider)
+    : CommandExecutorBase<WhichTodoCommand>(outputWriter), IWhichTodoCommandExecutor
 {
-    private readonly IAssemblyInformationProvider _assemblyInformationProvider;
-    private readonly IConstantsProvider _constantsProvider;
-
-    public WhichTodoCommandExecutor(IOutputWriter outputWriter,
-        IAssemblyInformationProvider assemblyInformationProvider,
-        IConstantsProvider constantsProvider)
-        : base(outputWriter)
-    {
-        _assemblyInformationProvider = assemblyInformationProvider;
-        _constantsProvider = constantsProvider;
-    }
-
-    public override void Execute(WhichTodoCommand command)
-    {
-        var sb = new StringBuilder()
-            .AppendLine($"Assembly location: {_assemblyInformationProvider.AssemblyLocation()}")
-            .AppendLine($"Todo version (commit): {_assemblyInformationProvider.GetCommitHash()}")
-            .AppendLine($"Build time: {_assemblyInformationProvider.GetBuildTime().ToString("yyyy-MM-dd HH:mm:ss")}")
-            .AppendLine($"DEBUG flag: {_assemblyInformationProvider.DebugFlag()}")
-            .AppendLine($"Process architecture: {RuntimeInformation.ProcessArchitecture}")
-            .AppendLine()
-            .AppendLine($"Framework version: {RuntimeInformation.FrameworkDescription}")
-            .AppendLine($"OS architecture: {RuntimeInformation.OSArchitecture}")
-            .AppendLine($"OS description: {RuntimeInformation.OSDescription}")
-            .AppendLine()
-            .AppendLine($"Project author: {_constantsProvider.ProjectAuthor} " + 
-                $"({_constantsProvider.ProjectAuthorContactDetails})")
-            .AppendLine($"Project website: {_constantsProvider.ProjectWebsite}")
-            .AppendLine();
-
-        OutputWriter.WriteLine(sb);
-    }
+    public override void Execute(WhichTodoCommand command) =>
+        OutputWriter.WriteLine(boilerPlateProvider.GetBoilerPlate());
 }
