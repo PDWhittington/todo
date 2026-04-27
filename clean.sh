661e9246fa7423ad7f5093f47adbb53bdffc2baf
Phil Whittington <philipwhittington@gmail.com>
2026-04-28 00:53:51 +0100

    Sorting scripts

--- clean.sh
+++ clean.sh
@@ -2,16 +2,19 @@
 
 echo "Deleting all bin folders . . ."
 
-if ! rm -rf **/bin/ 2>/dev/null; then
+if ! find . -type d -name bin -exec rm -rf {} +; then
   echo " ❌ Deleting bin folders failed."
   exit 1
 fi
 
 echo "Deleting all obj folders . . ."
 
-if ! rm -rf **/obj/ 2>/dev/null; then
+if ! find . -type d -name obj -exec rm -rf {} +; then
   echo " ❌ Deleting obj folders failed."
   exit 2
 fi
 
 echo "✅ All folders deleted successfully."
+
+find ./ -type d -name "bin"
+find ./ -type d -name "obj"
