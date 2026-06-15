f9e94898f5145f852acd67aa70157a180b5b3854
Philip Whittington <philipwhittington@gmail.com>
2026-06-15 14:58:21 +0100

    Make main Program.cs output stacktrace if an exception is thrown back that far (#382)

--- src/Todo/Program.cs
+++ src/Todo/Program.cs
@@ -17,6 +17,10 @@ catch (Exception e)
 {
     Console.WriteLine($"The app threw the following exception:{Environment.NewLine}{Environment.NewLine}");
     Console.WriteLine($"{e.GetType()}: {e.Message}");
+    Console.WriteLine();
+    Console.WriteLine("Stack trace:");
+    Console.WriteLine(e.StackTrace);
+    
 }
 finally
 {
