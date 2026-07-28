86bca2741c38ae1ef9378653359836510fbdb712
Phil Whittington <philipwhittington@gmail.com>
2026-07-28 03:39:14 +0100

    Making w map to which rather than web

--- src/Todo/CommandFactories/WhichTodoCommandFactory.cs
+++ src/Todo/CommandFactories/WhichTodoCommandFactory.cs
@@ -9,22 +9,23 @@ namespace Todo.CommandFactories;
 public class WhichTodoCommandFactory(IOutputWriter outputWriter)
     : CommandFactoryBase<WhichTodoCommand>(outputWriter, Words)
 {
-    private static readonly string[] Words = ["which"];
+    private static readonly string[] Words = ["w", "which"];
 
     public override bool IsDefaultCommandFactory => false;
 
     protected override string[] HelpText { get; } =
     [
-        "Reveals the location of the todo executable. Running this command is " +
-        "equivalent to invoking 'which todo' in bash, but can be run on any platform. " +
-        "In fact, a 'boiler plate' of information is produced."
+        "Reveals the location of the todo executable. Running this command is "
+            + "equivalent to invoking 'which todo' in bash, but can be run on any platform. "
+            + "In fact, a 'boiler plate' of information is produced.",
     ];
 
     protected override string Usage => "which";
 
     public override WhichTodoCommand? TryGetCommand(string commandLine)
     {
-        if (!IsThisCommand(commandLine, out var restOfCommand)) return null;
+        if (!IsThisCommand(commandLine, out var restOfCommand))
+            return null;
 
         return string.IsNullOrWhiteSpace(restOfCommand)
             ? WhichTodoCommand.Singleton
