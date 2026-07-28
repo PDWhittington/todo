48fea87493e3515443e75883215e5f426b77c614
Phil Whittington <philipwhittington@gmail.com>
2026-07-28 23:09:43 +0100

    Using OutputWriter throughout the app

--- src/Todo.Contracts/Services/ITodoService.cs
+++ src/Todo.Contracts/Services/ITodoService.cs
@@ -1,6 +1,10 @@
-﻿namespace Todo.Contracts.Services;
+﻿using Todo.Contracts.Services.UI;
+
+namespace Todo.Contracts.Services;
 
 public interface ITodoService
 {
+    IOutputWriter OutputWriter { get; }
+    IOutputWriterDisposableHandle InitialiseService();
     void PerformTask();
 }
